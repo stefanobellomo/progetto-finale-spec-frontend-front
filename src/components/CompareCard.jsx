09e5e1cc7057ca4600284c1c609eb7c469bea58f
stefanobellomo <stefanobellomo1998@gmail.com>
2026-03-09 12:11:21 +0100

    fix style and className

--- src/components/CompareCard.jsx
+++ src/components/CompareCard.jsx
@@ -1,18 +1,21 @@
 export default function CompareCard({ g }) {
     return (
-        <div key={g.id} className="compare-card">
-            <h3 className="game-title">{g.title}</h3>
-            <div>
-                <span className="details-label">Categoria:</span>
-                <span className="details-value">{g.category}</span>
+        <div className="compare-card">
+            <h3 className="compare-card-title">{g.title}</h3>
+
+            <div className="compare-row">
+                <span className="compare-label">Categoria:</span>
+                <span className="compare-value">{g.category}</span>
             </div>
-            <div>
-                <span className="details-label">spanPrezzo:</span>
-                <span className="details-value">{g.price}€</span>
+
+            <div className="compare-row">
+                <span className="compare-label">Prezzo:</span>
+                <span className="compare-value">{g.price}€</span>
             </div>
-            <div>
-                <span className="details-label">Valutazione:</span>
-                <span className="details-value">{g.rating}</span>
+
+            <div className="compare-row">
+                <span className="compare-label">Valutazione:</span>
+                <span className="compare-value">{g.rating}</span>
             </div>
         </div>
     )
